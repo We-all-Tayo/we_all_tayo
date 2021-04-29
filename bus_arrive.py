acd177fff76274d55eccc37330471dfcc49242d8
Pt J <peter.j@kakao.com>
2021-04-30 02:38:29 +0900

    Fix type error

--- bus_arrive.py
+++ bus_arrive.py
@@ -21,13 +21,13 @@ def get_bus_list(target_station):
     tree = ET.fromstring(response_body)
     bus_list = tree.find('msgBody').findall('itemList')
 
-    bus_come = []
+    bus_come = {}
 
     for bus in bus_list:
         #NOTE routeType: (1:공항, 2:마을, 3:간선, 4:지선,
         #  5:순환, 6:광역, 7:인천, 8:경기, 9:폐지, 0:공용)
 
         if (bus.find('arrmsg1').text == '곧 도착'):
-            bus_come.append({bus.find('rtNm').text: bus.find('routeType').text})
+            bus_come[bus.find("rtNm").text] = bus.find("routeType").text
         
     return bus_come
