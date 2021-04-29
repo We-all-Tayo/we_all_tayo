49c0852e4f295d05b91675eb33b9da824e1af0d8
Pt J <peter.j@kakao.com>
2021-04-30 02:32:43 +0900

    Modify hard-coded value to ratio

--- color_detection.py
+++ color_detection.py
@@ -16,20 +16,20 @@ UPPER_HSV = {
     'yellow': np.array([30, 255, 255], np.uint8),
 }
 
-def calculate_area(mask, image):
+def calculate_area(mask, image, image_area):
     result = 0
     kernal = np.ones((5, 5), 'uint8')
 
     # Create contour
     mask = cv2.dilate(mask, kernal)
     cv2.bitwise_and(image, image, mask=mask)
-    contours, hierarchy = cv2.findContours(
+    contours, _ = cv2.findContours(
         mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Track color
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
-        if (area > 300):
+        if (area > image_area * 0.001):
             _, _, w, h = cv2.boundingRect(contour)
             result += w*h
     
@@ -38,6 +38,10 @@ def calculate_area(mask, image):
 def detect_color(image_path, leftup=None, rightdown=None):
     # Get image and convert BGR color space to HSV color space
     image = cv2.imread(image_path)
+    height, width, _ = image.shape
+
+    image_area = height * width
+
     if leftup != None and rightdown != None:
          image = image[leftup[1]: rightdown[1], leftup[0]: rightdown[0]]
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -53,10 +57,10 @@ def detect_color(image_path, leftup=None, rightdown=None):
         LOWER_HSV['yellow'], UPPER_HSV['yellow'])
 
     # Calcuate color areas
-    red_area = calculate_area(red_mask, image)
-    green_area = calculate_area(green_mask, image)
-    blue_area = calculate_area(blue_mask, image)
-    yellow_area = calculate_area(yellow_mask, image)
+    red_area = calculate_area(red_mask, image, image_area)
+    green_area = calculate_area(green_mask, image, image_area)
+    blue_area = calculate_area(blue_mask, image, image_area)
+    yellow_area = calculate_area(yellow_mask, image, image_area)
 
     # Red : 6 광역 | Green: 4 지선 | Blue: 3 간선 | Yellow: 5 순환
     areas = {'6': red_area, '4': green_area, '3': blue_area, '5': yellow_area}
