3b46475476a198957ad140deb9346ca92e811437
Pt J <peter.j@kakao.com>
2021-05-21 14:23:28 +0900

    Reduce duplicated codes of color detection

--- color_detection.py
+++ color_detection.py
@@ -16,9 +16,16 @@ UPPER_HSV = {
     'yellow': np.array([30, 255, 255], np.uint8),
 }
 
-def calculate_area(mask, image, image_area):
+def calculate_area(image, image_area, color):
     result = 0
     kernal = np.ones((5, 5), 'uint8')
+
+    # Convert BGR color space to HSV color space
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define masks for each color
+    mask = cv2.inRange(hsv_image,
+        LOWER_HSV[color], UPPER_HSV[color])
 
     # Create contour
     mask = cv2.dilate(mask, kernal)
@@ -27,7 +34,7 @@ def calculate_area(mask, image, image_area):
         mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Track color
-    for pic, contour in enumerate(contours):
+    for contour in contours:
         area = cv2.contourArea(contour)
         if (area > image_area * 0.001):
             _, _, w, h = cv2.boundingRect(contour)
@@ -36,7 +43,7 @@ def calculate_area(mask, image, image_area):
     return result
 
 def detect_color(image_path, leftup=None, rightdown=None):
-    # Get image and convert BGR color space to HSV color space
+    # Get image
     image = cv2.imread(image_path)
     if leftup != None and rightdown != None:
          image = image[leftup[1]: rightdown[1], leftup[0]: rightdown[0]]
@@ -44,23 +51,11 @@ def detect_color(image_path, leftup=None, rightdown=None):
     height, width, _ = image.shape
     image_area = height * width
 
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # Define masks for each color
-    red_mask = cv2.inRange(hsv_image,
-        LOWER_HSV['red'], UPPER_HSV['red'])
-    green_mask = cv2.inRange(hsv_image,
-        LOWER_HSV['green'], UPPER_HSV['green'])
-    blue_mask = cv2.inRange(hsv_image,
-        LOWER_HSV['blue'], UPPER_HSV['blue'])
-    yellow_mask = cv2.inRange(hsv_image,
-        LOWER_HSV['yellow'], UPPER_HSV['yellow'])
-
     # Calcuate color areas
-    red_area = calculate_area(red_mask, image, image_area)
-    green_area = calculate_area(green_mask, image, image_area)
-    blue_area = calculate_area(blue_mask, image, image_area)
-    yellow_area = calculate_area(yellow_mask, image, image_area)
+    red_area = calculate_area(image, image_area, 'red')
+    green_area = calculate_area(image, image_area, 'green')
+    blue_area = calculate_area(image, image_area, 'blue')
+    yellow_area = calculate_area(image, image_area, 'yellow')
 
     # Red : 6 광역 | Green: 4 지선 | Blue: 3 간선 | Yellow: 5 순환
     areas = {'6': red_area, '4': green_area, '3': blue_area, '5': yellow_area}
