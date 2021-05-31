05423df29769b60b45cb791f865179349a9f6b47
knghyunwoo <kng.hyunwoo@gmail.com>
2021-05-31 10:17:06 +0900

    added several cases when detecting door

--- main.py
+++ main.py
@@ -70,8 +70,51 @@ def main(target_bus, target_station, img_path):
     # Angle Detection
     radian = detect_angle(img_path)
 
+    door_union = {}
+    door_union["x"] = min(door['x'], door_dict['x'])
+    door_union["y"] = min(door['y'], door_dict['y'])
+    door_union["right_x"] = max(door['x'] + door['w'], door_dict['x'] + door_dict['w'])
+    door_union["down_y"] = max(door['y'] + door['h'], door_dict['y'] + door_dict['h'])
+    door_union["w"] = door_union["right_x"] - door_union["x"]
+    door_union["h"] = door_union["down_y"] - door_union["y"]
+
+    door_intersect = {}
+    door_intersect["x"] = max(door['x'], door_dict['x'])
+    door_intersect["y"] = max(door['y'], door_dict['y'])
+    door_intersect["right_x"] = min(door['x'] + door['w'], door_dict['x'] + door_dict['w'])
+    door_intersect["down_y"] = min(door['y'] + door['h'], door_dict['y'] + door_dict['h'])
+    door_intersect["w"] = door_intersect["right_x"] - door_intersect["x"]
+    door_intersect["h"] = door_intersect["down_y"] - door_intersect["y"]
+
     # Calculation
-    distance, angle = calculate_distance_angle(door, radian)
+    distance1, angle1 = calculate_distance_angle(door, radian)  # opencv
+    distance2, angle2 = calculate_distance_angle(door_dict, radian)  # yolo
+    distance3, angle3 = calculate_distance_angle(door_union, radian)  # opencv + yolo union
+    distance4, angle4 = calculate_distance_angle(door_intersect, radian)  # opencv + yolo intersect
+
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.rectangle(img, (door['x'], door['y']), (door['x'] + door['w'], door['y'] + door['h']), color=(255, 0, 0),
+                  thickness=5)  # blue
+    cv2.rectangle(img, (door_dict['x'], door_dict['y']),
+                  (door_dict['x'] + door_dict['w'], door_dict['y'] + door_dict['h']), color=(0, 255, 0),
+                  thickness=5)  # green
+    cv2.rectangle(img, (door_union['x'], door_union['y']),
+                  (door_union['x'] + door_union['w'], door_union['y'] + door_union['h']), color=(0, 0, 255),
+                  thickness=5)  # red
+    cv2.rectangle(img, (door_intersect['x'], door_intersect['y']),
+                  (door_intersect['x'] + door_intersect['w'], door_intersect['y'] + door_intersect['h']),
+                  color=(255, 0, 255), thickness=5)  # pink
+    cv2.imwrite("test.png", img)
+
+    print(str(round(distance1 / 1000, 2)) + " meter")
+    print(str(round(angle1 * 180 / np.pi)) + " 도")
+    print(str(round(distance2 / 1000, 2)) + " meter")
+    print(str(round(angle2 * 180 / np.pi)) + " 도")
+    print(str(round(distance3 / 1000, 2)) + " meter")
+    print(str(round(angle3 * 180 / np.pi)) + " 도")
+    print(str(round(distance4 / 1000, 2)) + " meter")
+    print(str(round(angle4 * 180 / np.pi)) + " 도")
 
     return (
         str(round(distance / 1000, 2)) + " meter",
